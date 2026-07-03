ba783a141af533147e7f8eef657dbac307eb30f7
AftabAliRavji <aftab.ali.ravji@mail.com>
2026-07-03 14:44:52 +0100

    Add Stripe SDK and Composer metadata

---
+++ public/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a85b83dd039cd07af792984a9047674647dbb793',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a85b83dd039cd07af792984a9047674647dbb793',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v20.3.0',
+            'version' => '20.3.0.0',
+            'reference' => '266f0b05890172184cca66a0688abaf1a96b08d8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
